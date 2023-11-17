bce6e4147904ed6ea353f0ef438b7a0e457d58ea
renan <renan.stos077@gmail.com>
2023-11-17 11:52:20 -0300

    guessWord & fixes

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -8,32 +8,42 @@ let currentLine = 0;
 let currentLetter = 0;
 
 let formedWord = '';
+let chosenWord = '';
 
 chooseRandomWord();
+renderLetterBoxes();
 
-for (let i = 1; i <= MAX_LINES; i++ ) {
-  const line = document.createElement('div');
-  line.className = 'word-line';
+function chooseRandomWord() {
+  chosenWord = WORDS[Math.floor(Math.random() * WORDS.length)];
+  console.log(chosenWord);
+}
 
-  for (let j = 0; j < MAX_LETTERS; j++) {
-    const letter = document.createElement('div');
-    letter.className = 'word-letter';
-
-    line.appendChild(letter);
+function renderLetterBoxes() {
+  for (let i = 1; i <= MAX_LINES; i++ ) {
+    const line = document.createElement('div');
+    line.className = 'word-line';
+  
+    for (let j = 0; j < MAX_LETTERS; j++) {
+      const letter = document.createElement('div');
+      letter.className = 'word-letter';
+  
+      line.appendChild(letter);
+    }
+  
+    wordContainer.appendChild(line);
   }
-
-  wordContainer.appendChild(line);
 }
 
 onkeyup = (event) => {
   const keyCode = event.code;
   const key = event.key;
+  console.log(formedWord)
 
   if (keyCode.includes('Key')) {
     addLetter(key);
   } else if (key === 'Backspace') {
     removeLetter();
-  } else if (key === 'Enter') {
+  } else if (key === 'Enter' && formedWord.length === MAX_LETTERS) {
     guessWord();
   }
 }
@@ -44,6 +54,7 @@ function addLetter(key) {
   
   if (!currentLetterElement.innerHTML) {
     currentLetterElement.innerHTML = `<span>${key.toUpperCase()}</span>`;
+    formedWord+=key;
   }
 
   if (currentLetter < 4) {
@@ -71,14 +82,69 @@ function removeLetter() {
 
   if (currentLetterElement.innerHTML) {
     currentLetterElement.firstChild.remove();
+    formedWord = formedWord.slice(0, currentLetter);
   }
 }
 
 function guessWord() {
   const currentLineElement = getCurrentLineElement();
+  const children = currentLineElement.children;
+
+  const chosenWordNormalized = removeAccentsFromWord(chosenWord);
+  const formedWordLowerCase = formedWord.toLowerCase();
+
+  console.log(formedWordLowerCase);
+
+  if (formedWordLowerCase === chosenWordNormalized) {
+    applyRightLetterClass(children);
+    // chamar api do dicionario, 
+    // mostrando significado da palavra e frases prontas a utilizando
+
+    // mostrar botão play again // reseta tudo
+  } else {
+    //verificar letra por letra da formedWord com a chosenWord
+
+    for(let i = 0; i < formedWordLowerCase.length; i++) {
+      const formedWordLetter = formedWordLowerCase.charAt(i);
+      const chosenWordLetter = chosenWordNormalized.charAt(i);
+      const currentLetterBox = children.item(i);
+
+      if (formedWordLetter === chosenWordLetter) {
+        applyClassToElement(currentLetterBox, 'right-letter');
+      } else if (chosenWordNormalized.includes(formedWordLetter)) {
+        applyClassToElement(currentLetterBox, 'displaced-letter');
+      } else {
+        applyClassToElement(currentLetterBox, 'wrong-letter');
+      }
+
+    }
+
+    goToNextLine();
+  }
+  
 }
 
-function chooseRandomWord() {
-  const chosenWord = WORDS[Math.floor(Math.random() * WORDS.length)];
-  localStorage.setItem('chosenWord', chosenWord);
+function removeAccentsFromWord(word) {
+  return word.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
 }
+
+function applyRightLetterClass(children) {
+  Array.from(children).forEach(letterBox => {
+    applyClassToElement(letterBox, 'right-letter');
+  });
+}
+
+function applyClassToElement(el, className) {
+  el.classList.add(className);
+}
+
+function goToNextLine() {
+  if(currentLine < MAX_LINES) {
+    currentLine++;
+    currentLetter = 0;
+    formedWord = '';
+  } else {
+    //game over
+  }
+}
+
